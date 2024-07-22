a93c8dc94b5c92f776a45739ea5e00594d9aa849
Yulia Yanaeva <anaevaulia5@gmail.com>
2024-07-22 12:13:11 +0400

    add prototype for post method in endpoints

--- api/app.py
+++ api/app.py
@@ -1,1 +1,19 @@
-print('Hello, World!')
+from typing import Any
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/generate', methods=['POST'])
+def generate_qr_code():
+    info: dict[str, Any] = request.json
+    text_to_generate = info.get('text')
+
+    if isinstance(text_to_generate, str):
+        # TODO: Invoke func for QR-code generating
+        return jsonify("result"), 200
+    return "Await for a 'text' key in JSON request", 400
