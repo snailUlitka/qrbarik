a99a94c24dde93412b81699e505b8a616f2c939b
snail <snail1704@gmail.com>
2024-07-24 14:43:54 +0400

    Add start server from `app.py`
    - For run server now you can just exec `app.py`
    - Update `.gitignore` for work with my LSP

--- api/app.py
+++ api/app.py
@@ -6,8 +6,8 @@ import cv2
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 CORS(app)
 
 @app.route('/generate', methods=['POST'])
@@ -18,17 +18,19 @@ def generate_qr_code():
     if isinstance(text_to_generate, str):
         # TODO: Invoke func for QR-code generating
         return jsonify("result"), 200
-    
+
     return "Await for a 'text' key in JSON request", 400
 
 @app.route('/read', methods=['POST'])
 def read_qr_code():
     if 'file' not in request.files:
         return jsonify({'error': 'Await for image file'}), 400
-    
+
     file = request.files['file']
     image = Image.open(file.stream)
     image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) # Bytes array from the image
+
+    # TODO: Write your own solution for read QR codes
     det = cv2.QRCodeDetector()
     retval, decoded_info, _, _ = det.detectAndDecodeMulti(image_cv) # Detects qr-code on image
 
@@ -36,6 +38,9 @@ def read_qr_code():
     if retval:
         for info in decoded_info:
             results.append(info)
-    
+
     return jsonify({"text": results}), 200
-    
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=7845)
+
