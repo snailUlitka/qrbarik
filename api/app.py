9f8a58973865c44827318f85189a4b3c300664b7
Yulia <152644364+bloodlaac@users.noreply.github.com>
2024-07-24 14:01:02 +0400

    Api endpoints (#7)
    * add empty app.py
    
    - create venv
    - activate venv
    
    * add prototype for post method in endpoints
    
    * Add GET-method in endpoints
    
    * feat: update GET-method for endpoints
    
    - changed the GET method for qr-code reading to POST.
    - changed the format of the Base64 file
    received from the client to the standard binary
    
    * fix: add libs to requirements.txt, translate to EN
    line in read method

---
+++ api/app.py
@@ -0,0 +1,41 @@
+from typing import Any
+from PIL import Image
+import numpy as np
+import cv2
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/generate', methods=['POST'])
+def generate_qr_code():
+    info: dict[str, Any] = request.json
+    text_to_generate = info.get('text')
+
+    if isinstance(text_to_generate, str):
+        # TODO: Invoke func for QR-code generating
+        return jsonify("result"), 200
+    
+    return "Await for a 'text' key in JSON request", 400
+
+@app.route('/read', methods=['POST'])
+def read_qr_code():
+    if 'file' not in request.files:
+        return jsonify({'error': 'Await for image file'}), 400
+    
+    file = request.files['file']
+    image = Image.open(file.stream)
+    image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) # Bytes array from the image
+    det = cv2.QRCodeDetector()
+    retval, decoded_info, _, _ = det.detectAndDecodeMulti(image_cv) # Detects qr-code on image
+
+    results = []
+    if retval:
+        for info in decoded_info:
+            results.append(info)
+    
+    return jsonify({"text": results}), 200
+    
