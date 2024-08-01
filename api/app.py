1f36d76163ec7f5cc8b669c41b9dd25c5c7526a3
bloodlaac <anaevaulia5@gmail.com>
2024-08-01 17:32:37 +0400

    fix: fix /generate and /read methods
    added error description for invalid input parameters

--- api/app.py
+++ api/app.py
@@ -32,24 +32,29 @@ swagger_config = {
 swagger = Swagger(app, config=swagger_config)
 
 @app.route('/generate', methods=['POST'])  
-@swag_from('documentation/generate.yml')
+@swag_from('swagger/generate.yml')
 def generate_qr_code():
     info: dict[str, Any] = request.get_json()
     text_to_generate = info.get('text')
 
+    if "text" not in request.get_json():
+        return jsonify("Await for a 'text' key in JSON request"), 400
+
     if isinstance(text_to_generate, str):
         # TODO: Invoke func for QR-code generating
+        if (text_to_generate == "" or text_to_generate.isspace()):
+             return jsonify("Invalid value in 'text' key. An empty string is not allowed"), 400
         return jsonify("result"), 200
-
-    return "Await for a 'text' key in JSON request", 400
+    
+    return jsonify("Invalid value in 'text' key"), 400
 
 @app.route('/read', methods=['POST'])
-@swag_from('documentation/read.yml')
+@swag_from('swagger/read.yml')
 def read_qr_code():
-    if 'file' not in request.files:
-        return jsonify({'error': 'Await for image file'}), 400
+    if "file" not in request.files:
+        return jsonify({"error": "Await for image file"}), 400
 
-    file = request.files['file']
+    file = request.files["file"]
     image = Image.open(file.stream)
     image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) # Bytes array from the image
 
@@ -57,9 +62,11 @@ def read_qr_code():
     det = cv2.QRCodeDetector()
     retval, decoded_info, _, _ = det.detectAndDecodeMulti(image_cv) # Detects qr-code on image
 
+    if not retval:
+        return jsonify({"error": "No QR-code detected on the image"}), 400
+
     results = []
-    if retval:
-        for info in decoded_info:
+    for info in decoded_info:
             results.append(info)
 
     return jsonify({"text": results}), 200
