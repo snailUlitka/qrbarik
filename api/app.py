e6cf11743ae46c787fd07752c87f0121e6221d3c
Yulia <152644364+bloodlaac@users.noreply.github.com>
2024-08-01 19:13:53 +0400

    API documentation (#10)
    * docs: add api documentation
    
    - added documentation file API.yml
    - set up decorators and configuration for methods in app.py
    - updated requirements
    
    * fix: change path to relative,
    change examples with plain text
    
    * fix: fix incorrect path to file,
    add instructions to README.md,
    fix specifying version in requirements.txt
    
    * fix: fix parameters in documentation
    
    - added generate.yml and read.yml files
    - updated paths in methods in app.py
    
    * remove file API.yml
    
    * fix: fix 415 error in /generate method
    
    * fix: fix /generate and /read methods
    
    added error description for invalid input parameters

--- api/app.py
+++ api/app.py
@@ -5,28 +5,56 @@ import cv2
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flasgger import Swagger, swag_from
 
 
 app = Flask(__name__)
 CORS(app)
+app.config['SWAGGER'] = {
+    'title': 'QR Code API',
+    'uiversion': 3
+}
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'swagger',
+            "route": '/swagger.json',
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/swagger"
+}
 
-@app.route('/generate', methods=['POST'])
+swagger = Swagger(app, config=swagger_config)
+
+@app.route('/generate', methods=['POST'])  
+@swag_from('swagger/generate.yml')
 def generate_qr_code():
-    info: dict[str, Any] = request.json
+    info: dict[str, Any] = request.get_json()
     text_to_generate = info.get('text')
+
+    if "text" not in request.get_json():
+        return jsonify("Await for a 'text' key in JSON request"), 400
 
     if isinstance(text_to_generate, str):
         # TODO: Invoke func for QR-code generating
+        if (text_to_generate == "" or text_to_generate.isspace()):
+             return jsonify("Invalid value in 'text' key. An empty string is not allowed"), 400
         return jsonify("result"), 200
-
-    return "Await for a 'text' key in JSON request", 400
+    
+    return jsonify("Invalid value in 'text' key"), 400
 
 @app.route('/read', methods=['POST'])
+@swag_from('swagger/read.yml')
 def read_qr_code():
-    if 'file' not in request.files:
-        return jsonify({'error': 'Await for image file'}), 400
+    if "file" not in request.files:
+        return jsonify({"error": "Await for image file"}), 400
 
-    file = request.files['file']
+    file = request.files["file"]
     image = Image.open(file.stream)
     image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) # Bytes array from the image
 
@@ -34,9 +62,11 @@ def read_qr_code():
     det = cv2.QRCodeDetector()
     retval, decoded_info, _, _ = det.detectAndDecodeMulti(image_cv) # Detects qr-code on image
 
+    if not retval:
+        return jsonify({"error": "No QR-code detected on the image"}), 400
+
     results = []
-    if retval:
-        for info in decoded_info:
+    for info in decoded_info:
             results.append(info)
 
     return jsonify({"text": results}), 200
